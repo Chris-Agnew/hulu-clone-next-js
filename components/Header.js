2cec00af1522e795804304a2e154f78caa7871c1
Chris <chris@chris-agnew.com>
2021-04-21 18:47:37 -0400

    ADD: Finished the Header component

--- components/Header.js
+++ components/Header.js
@@ -7,23 +7,27 @@ import {
   UserIcon,
 } from "@heroicons/react/outline";
 import Image from "next/image";
-import HeaderItem from "./HeaderItem";
+import HeaderItem from "../components/HeaderItem";
 
-const Header = () => {
+function Header() {
   return (
-    <header>
-      <div>
+    <header className="flex flex-col sm:flex-row m-5 justify-between items-center h-auto">
+      <div className="flex flex-grow justify-evenly max-w-2xl">
         <HeaderItem title="HOME" Icon={HomeIcon} />
+        <HeaderItem title="TRENDING" Icon={LightningBoltIcon} />
+        <HeaderItem title="VERIFIED" Icon={BadgeCheckIcon} />
+        <HeaderItem title="COLLECTIONS" Icon={CollectionIcon} />
+        <HeaderItem title="SEARCH" Icon={SearchIcon} />
+        <HeaderItem title="ACCOUNT" Icon={UserIcon} />
       </div>
-      <h1>This is the Header</h1>
       <Image
         className="object-contain"
         src="https://links.papareact.com/ua6"
         width={200}
-        height={200}
+        height={100}
       />
     </header>
   );
-};
+}
 
 export default Header;
