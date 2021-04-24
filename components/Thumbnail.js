57dfe2cda8421997a15b8b6ac725ddb73097d2db
Chris <chris@chris-agnew.com>
2021-04-24 12:46:49 -0400

    Fixed: Bugs on large screens

--- components/Thumbnail.js
+++ components/Thumbnail.js
@@ -1,9 +1,10 @@
-import Image from "next/image";
 import { ThumbUpIcon } from "@heroicons/react/outline";
+import Image from "next/image";
 import { forwardRef } from "react";
 
-const Thumbnail = forwardRef(({ result, ref }) => {
-  const BASE_URL = "https://image.tmdb.org/t/p/original";
+const Thumbnail = forwardRef(({ result }, ref) => {
+  const BASE_URL = "https://image.tmdb.org/t/p/original/";
+
   return (
     <div
       ref={ref}
@@ -18,16 +19,17 @@ const Thumbnail = forwardRef(({ result, ref }) => {
         height={1080}
         width={1920}
       />
-      <div>
+
+      <div className="p-2">
         <p className="truncate max-w-md">{result.overview}</p>
-        <h2 className="mt-1 text-2xl text-white transition-all duration-100 ease-in-out group-hover:font-bold ">
+
+        <h2 className="mt-1 text-2xl text-white transition-all duration-100 ease-in-out group-hover:font-bold">
           {result.title || result.original_name}
         </h2>
-        <p className="flex items-center opacity-0 group-hover:opacity-100">
-          {result.media_type && `${result.media_type} ·`}{" "}
-          {result.release_date || result.first_air_date} ·{" "}
-          <ThumbUpIcon className="h-5 mx-2" />
-          {result.vote_count}
+        <p className="opacity-0 group-hover:opacity-100 flex items-center">
+          {result.media_type && `${result.media_type} •`}{" "}
+          {result.release_date || result.first_air_date} •{" "}
+          <ThumbUpIcon className="h-5 mx-2" /> {result.vote_count}
         </p>
       </div>
     </div>
