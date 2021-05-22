774622378a16a081a857cb6d1cc2df4b98deb08c
Chris <chris@chris-agnew.com>
2021-05-21 21:32:22 -0400

    Add: Firebase

---
+++ utils/firebaseConfig.js
@@ -0,0 +1,13 @@
+import firebase from "firebase/app";
+import "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA4BipbMOXANl4Wv2XchAHUpJzHq_1ea2c",
+  authDomain: "hulu-app-858a9.firebaseapp.com",
+  projectId: "hulu-app-858a9",
+  storageBucket: "hulu-app-858a9.appspot.com",
+  messagingSenderId: "250505922869",
+  appId: "1:250505922869:web:56689f65e5482bb93ce47c",
+};
+
+firebase.initializeApp(firebaseConfig);
